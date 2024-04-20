5b91a55f1ca85ff1a00a47a799f3d63af32f2fa0
Ted Melot <tmelot@gmail.com>
2024-04-20 10:19:04 -0400

    Add more lexer unit tests

--- lexer_test.go
+++ lexer_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 
 func runLexerWithStr(s string) ([]Token, error) {
@@ -40,5 +42,39 @@ func TestLexerInvalidJson(t *testing.T) {
 	_, err = runLexerWithStr("/")
 	if err == nil {
 		t.Error("Expected an error, did not error")
+	}
+}
+
+func TestLexerTokenCounts(t *testing.T) {
+	// Test with strings
+	result, _ := runLexerWithStr(`{"one": "two"}`)
+	if len(result) != 5 {
+		t.Errorf("Expected to lex 5 tokens, lexed %d instead", len(result))
+	}
+
+	// Test with numbers & strings
+	result, _ = runLexerWithStr(`{"one": 1.111, "two": 2.222}`)
+	if len(result) != 9 {
+		t.Errorf("Expected to lex 9 tokens, lexed %d instead", len(result))
+	}
+
+	// Test mixed whitespace
+	result, _ = runLexerWithStr(`{   	 	  "one": 1}`)
+	if len(result) != 5 {
+		t.Errorf("Expected to lex 5 tokens, lexed %d instead", len(result))
+	}
+
+	// Test newlines
+	result, _ = runLexerWithStr(`{
+		  "one":
+		 1 		}	 	`)
+	if len(result) != 5 {
+		t.Errorf("Expected to lex 5 tokens, lexed %d instead", len(result))
+	}
+
+	// Test nested tokens
+	result, _ = runLexerWithStr(`{"rankings": [{"name": "Smash Ultimate", "rank": 1}, {"name": "Noita", "rank": 2}]}`)
+	if len(result) != 25 {
+		t.Errorf("Expected to lex 25 tokens, lexed %d instead", len(result))
 	}
 }

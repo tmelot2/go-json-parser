659d55362fa813cadef0fecdffd40fa16f2a101d
tmelot2 <tmelot@gmail.com>
2024-06-15 15:12:37 -0400

    Replace hardcoded profile points with a simple block-based profiler, remove unused file, cleanup

--- haversine-compute-real-json-parser.go
+++ haversine-compute-real-json-parser.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	// "strings"
 	// "strconv"
+	// "time"
 
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
@@ -34,14 +35,6 @@ func GetPrinter() *message.Printer {
 	return message.NewPrinter(language.English) // For printing large numbers with commas
 }
 
-func PrintTimeElapsed(label string, totalTSCElapsed, start, end uint64) {
-	elapsed := end - start
-	percent := float64(100.0 * (float64(elapsed) / float64(totalTSCElapsed)))
-	p := GetPrinter()
-	s := p.Sprintf("%*d", OUTPUT_WIDTH, elapsed)
-	p.Printf("  %s: %14s (%.2f%%)\n", label, s, percent)
-}
-
 var DEBUG = false
 var OUTPUT_WIDTH = 10
 
@@ -49,52 +42,43 @@ var OUTPUT_WIDTH = 10
 // Main
 //
 func main() {
-	// Profiling vars
-	var (
-		prof_begin         = uint64(0)
-		prof_read          = uint64(0)
-		prof_read_tostr    = uint64(0)
-		prof_miscSetup     = uint64(0)
-		prof_parse         = uint64(0)
-		prof_sum           = uint64(0)
-		prof_miscOutput    = uint64(0)
-		prof_end          = uint64(0)
-		// TODO: Add steps for JsonValue stuff, even though it's mixed around
-	)
+	prof := newProfiler()
+	prof.Start("Startup")
+
 	// Setup
 	const EARTH_RADIUS = 6372.8
 	p := GetPrinter()
 
-	// cpuFreq := EstimateCPUTimerFreq(true)
-
 	// Get input args
 	inputFileArg := flag.String("input", "pairs.json", "Name of input file containing point pairs")
 	flag.Parse()
+	prof.End("Startup")
 
-	prof_begin = ReadCPUTimer()
 
 	// Read JSON file, convert to string
-	prof_read = ReadCPUTimer()
+	prof.Start("Read")
 	data, err := os.ReadFile(*inputFileArg)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	prof_read_tostr = ReadCPUTimer()
+	prof.End("Read")
+	prof.Start("ReadToStr")
 	strData := string(data)
-	prof_miscSetup = ReadCPUTimer()
+	prof.End("ReadToStr")
 	DebugPrintln(strData)
 
 	// Parse
-	prof_parse = ReadCPUTimer()
+	prof.Start("Parse")
 	jsonResult, err := ParseJson(strData)
 	if err != nil {
 		fmt.Println("Error parsing JSON:", err)
 		return
 	}
+	prof.End("Parse")
 
 	// Loop over JSON to do stuff
-	prof_sum = ReadCPUTimer()
+	prof.Start("Sum")
 	fmt.Println("===============================")
 	haversineSum := 0.0
 	pairs, _ := jsonResult.GetArray("pairs")
@@ -106,24 +90,11 @@ func main() {
 		haversineSum += referenceHaversine(x0, y0, x1, y1, EARTH_RADIUS)
 	}
 	avg := haversineSum / float64(len(pairs))
+	prof.End("Sum")
 
-	prof_miscOutput = ReadCPUTimer()
+	prof.Start("MiscOutput")
 	p.Printf("Count: %*d\nHaversine sum: %.16f\nHaversine avg: %.16f\n", 14, len(pairs), haversineSum, avg)
+	prof.End("MiscOutput")
 
-	prof_end = ReadCPUTimer()
-
-	// Calculate & print profile timings
-	totalCPUElapsed := prof_end - prof_begin
-
-	cpuFreq := EstimateCPUTimerFreq(false)
-	ms := 1000.0 * float64(totalCPUElapsed) / float64(cpuFreq)
-	fmt.Println("\n[CPU profiling stats]")
-	p.Printf("Total time: %0.4fms (CPU freq %*d)\n", ms, 14, cpuFreq)
-	PrintTimeElapsed("Startup     ", totalCPUElapsed, prof_begin, prof_read)
-	PrintTimeElapsed("Read        ", totalCPUElapsed, prof_read, prof_read_tostr)
-	PrintTimeElapsed("Read ToStr  ", totalCPUElapsed, prof_read_tostr, prof_miscSetup)
-	PrintTimeElapsed("Misc Setup  ", totalCPUElapsed, prof_miscSetup, prof_parse)
-	PrintTimeElapsed("Parse       ", totalCPUElapsed, prof_parse, prof_sum)
-	PrintTimeElapsed("Sum         ", totalCPUElapsed, prof_sum, prof_miscOutput)
-	PrintTimeElapsed("Misc Output ", totalCPUElapsed, prof_miscOutput, prof_end)
+	prof.Print()
 }
---
+++ profiler.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TSC struct {
+	start uint64
+	end   uint64
+}
+
+type Profiler struct {
+	// Maps block names to TSC start & end counters.
+	tscs   map[string]TSC
+	// Maps block names to total counted time from TSCs
+	totals map[string]uint64
+	// Remembers ordering of TSCs by block name
+	order  []string
+}
+
+func newProfiler() *Profiler {
+	return &Profiler{
+		tscs:   make(map[string]TSC),
+		totals: make(map[string]uint64),
+		order:  make([]string, 0),
+	}
+}
+
+// Starts a new time stamp counter for the given block name.
+// NOTE: WILL ERASE existing data for the given block.
+func (p *Profiler) Start(name string) {
+	// Add to ordering
+	found := false
+	for _, o := range p.order {
+		if o == name {
+			found = true
+			break
+		}
+	}
+	if !found {
+		p.order = append(p.order, name)
+	}
+
+	// Timestamp start of measurement
+	p.tscs[name] = TSC{
+		start: ReadCPUTimer(),
+		end:   0,
+	}
+}
+
+// Ends a time stamp counter & accumulates its total duration.
+func (p *Profiler) End(name string) {
+	// Ignore if TSC block name does not exist
+	tscVal, ok := p.tscs[name]
+	if !ok {
+		fmt.Printf("profiler.end(): Block %s does not exist, ignoring\n", name)
+		return
+	}
+
+	tscVal.end = ReadCPUTimer()
+	p.tscs[name] = tscVal
+
+	// Add sum for block if it doesn't exist
+	totalVal, ok := p.totals[name]
+	if !ok {
+		p.totals[name] = 0
+		totalVal = 0
+	}
+
+	// Calculate total
+	if tscVal.start != 0 && tscVal.end != 0 {
+		// TODO: Check that end > start
+		duration := tscVal.end - tscVal.start
+		fmt.Printf("profiler.end(): block %s, adding sum, current = %d, duration = %d, new = %d\n", name, totalVal, duration, totalVal+duration)
+		totalVal += duration
+		p.totals[name] = totalVal
+	}
+}
+
+// Prints block names & their durations in the order that Start() was called for each block.
+func (p *Profiler) Print() {
+	// Compute total
+	totalCycles := uint64(0)
+	for _, t := range p.totals {
+		totalCycles += t
+	}
+
+	// Print CPU info
+	printer := GetPrinter()
+	cpuFreq := EstimateCPUTimerFreq(false)
+	ms := 1000.0 * float64(totalCycles) / float64(cpuFreq)
+	fmt.Println("\n[CPU profiling stats]")
+	printer.Printf("Total time: %0.4fms (CPU freq %*d)\n", ms, 14, cpuFreq)
+
+	// Print block profiles
+	for _, blockName := range p.order {
+		p.PrintBlockTimeElapsed(blockName, p.totals[blockName], totalCycles)
+	}
+}
+
+// Returns len of longest block name string. Used for print formatting.
+func (p *Profiler) GetLongestBlockNameLen() int {
+	longest := 0
+	for _,b := range p.order {
+		if len(b) > longest {
+			longest = len(b)
+		}
+	}
+	return longest
+}
+
+// Prints profiling information for the given block data.
+func (p *Profiler) PrintBlockTimeElapsed(label string, durationCycles, totalCycles uint64) {
+	printer := GetPrinter()
+	durationCyclesStr := printer.Sprintf("%*d", 20, durationCycles)
+	longestBlockNameLen := p.GetLongestBlockNameLen() + 1
+	percent := float64(100.0 * (float64(durationCycles) / float64(totalCycles)))
+
+	printer.Printf("  %*s: %14s (%.2f%%)\n", longestBlockNameLen, label, durationCyclesStr, percent)
+}

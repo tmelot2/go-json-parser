2089ecf8421c619088b402517ae2078c9b6b4129
Ted Melot <tmelot@gmail.com>
2024-06-17 18:47:28 -0400

    Update Linux timer vars to uin64, make it work!

--- timer_linux.go
+++ timer_linux.go
@@ -1,53 +1,61 @@
-// +build linux
+// +build darwin
 
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/sys/unix"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 // Declare asm functions that return CPU timer values.
 // The ARM impl uses CNTVCT which returns a 24MHz timer on MacOS Sonoma.
-// Without a cycle counter from the CPU & OS, the 24Mhz doesn't really help us.
-func ReadCPUTimer() int64
+// Without a cycle counter from the CPU & OS, the 24Mhz doesn't really help us
+// count cycles. BUT, the relative percentages of cycle time is still useful!
+func ReadCPUTimer() uint64
 
 // GetOSTimerFreq returns the frequency of the OS timer.
-func GetOSTimerFreq() (int64, error) {
+func GetOSTimerFreq() (uint64, error) {
     // On Unix-like systems, the frequency can be considered as nanoseconds in a second
     return 1e9, nil // 1 second = 1e9 nanoseconds
 }
 
 // ReadOSTimer returns the current time from the OS high-resolution timer.
-func ReadOSTimer() (int64, error) {
+func ReadOSTimer() (uint64, error) {
     var ts unix.Timespec
     err := unix.ClockGettime(unix.CLOCK_MONOTONIC, &ts)
     if err != nil {
         return 0, err
     }
     osTimerFreq, _ := GetOSTimerFreq()
-    return osTimerFreq * ts.Sec + ts.Nsec, nil // Convert to nanoseconds
+    return osTimerFreq * uint64(ts.Sec) + uint64(ts.Nsec), nil // Convert to nanoseconds
 }
 
-func PrintTimerStats() {
+// Prints read, measurement, & guess of CPU frequency & related data.
+func EstimateCPUTimerFreq(printDebug bool) uint64 {
 	// Setup
-	millisecondsToWait := int64(10)
-	// TODO: Optionally get ms from input args
-	width := 24 // Output width
+	millisecondsToWait := uint64(100)
+	width := 20 // Output width
 	p := message.NewPrinter(language.English) // For printing large numbers with commas
 
+	// Get OS timer frequency
 	osFreq, err := GetOSTimerFreq()
 	if err != nil {
 		fmt.Println("Error getting OS timer frequency:", err)
-		return
+		return 0
 	}
 	// In nanoseconds per second
-	p.Printf("OS Timer Frequency [reported]:          %*d\n", width, osFreq)
+	if printDebug {
+		p.Printf("OS Timer Frequency [reported]: %*d\n", width, osFreq)
+	}
 
 	cpuStart   := ReadCPUTimer()
 	osStart, _ := ReadOSTimer()
 
-	var osEnd int64
-	var osElapsed int64
+	var osEnd uint64
+	var osElapsed uint64
 	osWaitTime := osFreq * millisecondsToWait / 1000
 	for osElapsed < osWaitTime {
 		osEnd, _ = ReadOSTimer()
@@ -56,16 +64,18 @@ func PrintTimerStats() {
 	cpuEnd := ReadCPUTimer()
 	cpuElapsed := cpuEnd - cpuStart
 
-	cpuFreq := int64(0)
+	cpuFreq := uint64(0)
 	if osElapsed > 0 {
 		cpuFreq = osFreq * cpuElapsed / osElapsed
 	}
 
-	// p.Printf(  "OS Timer:      %*d -> %*d = %*d elapsed\n", width, osStart, width, osEnd, width, osElapsed)
-	p.Printf("OS Timer:                               %*d elapsed\n", width, osElapsed)
-	p.Printf("OS Seconds (elapsed/freq):                   %*.4f\n", width, float64(osElapsed) / float64(osFreq))
+	if printDebug {
+		p.Printf("OS Timer:                      %*d elapsed\n", width, osElapsed)
+		p.Printf("OS Seconds (elapsed/freq):          %*.4f\n", width, float64(osElapsed) / float64(osFreq))
 
-	// p.Printf(  "CPU timer:     %*d -> %*d = %*d elapsed\n", width, cpuStart, width, cpuEnd, width, cpuElapsed)
-	p.Printf("CPU timer:                              %*d elapsed\n", width, cpuElapsed)
-	p.Printf("CPU freq (guessed):                     %*d\n", width, cpuFreq)
- }
+		p.Printf("CPU timer:                     %*d elapsed\n", width, cpuElapsed)
+		p.Printf("CPU freq (guessed):            %*d\n", width, cpuFreq)
+	}
+
+	return uint64(cpuFreq)
+}

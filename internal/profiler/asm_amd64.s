3e0c07639048edb2df6129e2bcda1953e62b83c9
tmelot2 <tmelot@gmail.com>
2024-06-26 10:08:27 -0400

    Cleanup & document assembly

--- internal/profiler/asm_amd64.s
+++ internal/profiler/asm_amd64.s
@@ -1,6 +1,5 @@
-// Copyright 2016 David Terei.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Original assembly by David Terei: https://github.com/dterei/gotsc
+// I unified the begin/end calls into 1 single call.
 
 #include "textflag.h"
 
@@ -12,23 +11,3 @@ TEXT ·ReadCPUTimer(SB),NOSPLIT,$0-8
 	ADDQ	DX, AX
 	MOVQ	AX, ret+0(FP)
 	RET
-
-// func ReadCpuTimerStart() uint64
-// TEXT ·ReadCpuTimerStart(SB),NOSPLIT,$0-8
-// 	CPUID
-// 	RDTSC
-// 	SHLQ	$32, DX
-// 	ADDQ	DX, AX
-// 	MOVQ	AX, ret+0(FP)
-// 	RET
-
-// func BenchEnd() uint64
-// TEXT ·ReadCpuTimerEnd(SB),NOSPLIT,$0-8
-// 	BYTE	$0x0F // RDTSCP
-// 	BYTE	$0x01
-// 	BYTE	$0xF9
-// 	SHLQ	$32, DX
-// 	ADDQ	DX, AX
-// 	MOVQ	AX, ret+0(FP)
-// 	CPUID
-// 	RET

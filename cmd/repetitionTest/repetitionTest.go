272ec99051ebd61fc31b41563e7ffe4f19fb4cf4
tmelot2 <tmelot@gmail.com>
2024-06-26 09:35:42 -0400

    Add multiple test funcs per testing round

--- cmd/repetitionTest/repetitionTest.go
+++ cmd/repetitionTest/repetitionTest.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"tmelot.jsonparser/internal/profiler"
 	"tmelot.jsonparser/internal/repetitionTester"
 )
 
-func readViaOSStat(rt *repetitionTester.RepetitionTester, fileName string, byteCount uint64) {
+func readViaOSReadFile(rt *repetitionTester.RepetitionTester, fileName string, byteCount uint64) {
 	for rt.IsTesting() {
 		// Read file
 		rt.BeginTime()
@@ -23,12 +26,67 @@ func readViaOSStat(rt *repetitionTester.RepetitionTester, fileName string, byteC
 	}
 }
 
+func readViaIOUtilReadFile(rt *repetitionTester.RepetitionTester, fileName string, byteCount uint64) {
+	for rt.IsTesting() {
+		// Read file
+		rt.BeginTime()
+		_, err := ioutil.ReadFile(fileName)
+		rt.EndTime()
+		if err != nil {
+			msg := fmt.Sprintln("Error:", err)
+			panic(msg)
+		}
+		rt.CountBytes(byteCount)
+	}
+}
+
+func readViaBufIOReader(rt *repetitionTester.RepetitionTester, fileName string, byteCount uint64) {
+	for rt.IsTesting() {
+		// Read file
+		file, err := os.Open(fileName)
+		if err != nil {
+			msg := fmt.Sprintln("Error:", err)
+			panic(msg)
+		}
+		reader := bufio.NewReader(file)
+
+		rt.BeginTime()
+		_, err = io.ReadAll(reader)
+		rt.EndTime()
+		if err != nil {
+			msg := fmt.Sprintln("Error:", err)
+			panic(msg)
+		}
+		rt.CountBytes(byteCount)
+	}
+}
+
+type ReadOverheadTestFunc func(*repetitionTester.RepetitionTester, string, uint64)
+type TestFunction struct {
+	name string
+	fun  ReadOverheadTestFunc
+}
+
+
 func main() {
 	fileNameArg := flag.String("fileName", "../../pairs.json", "Path to pairs JSON file")
 	flag.Parse()
 	fileName := *fileNameArg
 
-	rt := repetitionTester.NewRepetitionTester()
+	// Table of test functions to test.
+	testFunctions := [3]TestFunction{
+		{name: "OS.ReadFile", fun: readViaOSReadFile},
+		{name: "ioutil.ReadFile", fun: readViaIOUtilReadFile},
+		{name: "bufio.Reader", fun: readViaBufIOReader},
+	}
+
+	// Create multiple testers, one for each test function.
+	var testers [len(testFunctions)]*repetitionTester.RepetitionTester
+	for i,_ := range testers {
+		testers[i] = repetitionTester.NewRepetitionTester()
+	}
+
+	// rt := repetitionTester.NewRepetitionTester()
 	cpuFreq := profiler.EstimateCPUTimerFreq(false)
 
 	// Get file size for bandwidth calculation purposes.
@@ -38,11 +96,15 @@ func main() {
 	}
 	byteCount := uint64(fileInfo.Size())
 
+	// Run tests!
 	for i := 0; i < 3; i++ {
-		// bytes := uint64(0)
-		secondsToTry := uint32(2)
-		rt.NewTestWave(byteCount, cpuFreq, secondsToTry)
-		readViaOSStat(rt, fileName, byteCount)
+		for i, testFunc := range testFunctions {
+			fmt.Println(testFunc.name, ":")
+			secondsToTry := uint32(2)
+			testers[i].NewTestWave(byteCount, cpuFreq, secondsToTry)
+			testFunc.fun(testers[i], fileName, byteCount)
+			fmt.Println("")
+		}
 		fmt.Println("=========================================")
 	}
 
